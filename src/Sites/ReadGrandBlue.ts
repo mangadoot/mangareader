10f980397a2400677bb1243d735ae8def7062511
mangadoot <93862925+mangadoot@users.noreply.github.com>
2022-07-27 19:23:41 +0200

    feat: improve reading on readgrandblue

--- src/Sites/ReadGrandBlue.ts
+++ src/Sites/ReadGrandBlue.ts
@@ -14,11 +14,32 @@ export default class ReadGrandBlue extends AbstractSite {
   }
 
   protected handleSite(): void {
-    this.logger.info('just css for now...');
     styles.use();
-    this.imageNavigatorFactory.create(
-      Array.from(document.querySelectorAll('.entry-content img')).filter(is(HTMLImageElement)),
+
+    // remove the ad-divs
+    for (const element of document.querySelectorAll('.entry-content div.code-block')) {
+      element.remove();
+    }
+    // remove empty tags so they wont make the layout look weird
+    for (const element of document.querySelectorAll('.entry-content p:empty')) {
+      element.remove();
+    }
+
+    const images: HTMLImageElement[] = Array.from(document.querySelectorAll('.entry-content img')).filter(
+      is(HTMLImageElement),
     );
+    for (const image of images) {
+      image.addEventListener('error', () => {
+        if (image.dataset.reloadTries !== undefined && Number(image.dataset.reloadTries) > 3) {
+          return;
+        }
+        this.logger.error(`failed to load image ${image.src} - try to reload`);
+        image.dataset.reloadTries = String(Number(image.dataset.reloadTries || 0) + 1);
+        // TODO: not sure this actually works..
+        image.src = image.src;
+      });
+    }
+    this.imageNavigatorFactory.create(images);
   }
 
   protected shouldHandleSite(url: URL): boolean {
